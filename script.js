d23fc537bea64fcb39b5f3ca6f2e546e18f61529
Vitor <vitorm.ramires@hotmail.com>
2021-04-27 18:41:09 -0300

    corrigindo botoes

--- script.js
+++ script.js
@@ -3,31 +3,10 @@
         var botao = document.getElementById('botao')
         var quadradoP = document.querySelectorAll('#squareP')
         var quadrado = document.querySelectorAll('.squarelist')
-        var display = document.querySelector('.display')
-        var corrige = document.querySelector('.corrige')
-        var confirma = document.querySelector('.confirma')
-      
-
-
-/*function getBotao(number){
-         if(quadradoP[0].innerHTML == ""){
-         quadradoP[0].innerHTML = number 
-         return
-                } 
-                if(quadradoP[0].innerHTML != "" && quadradoP[1].innerHTML == ""){        
-                        quadradoP[1].innerHTML = number 
-                        return
-                } 
-                if(quadradoP[1].innerHTML != "" && quadradoP[2].innerHTML == ""){
-                        quadradoP[2].innerHTML = number 
-                        return
-                } 
-                if(quadradoP[2].innerHTML != "" && quadradoP[3].innerHTML == ""){
-                quadradoP[3].innerHTML = number 
-                return
-        } 
-        
-}*/
+        var tela = document.querySelector('.display')
+        var Corrigir = document.querySelector('.Corrigir')
+        var emBranco = document.querySelector('.emBranco')
+        var Confirmar = document.querySelector('.Confirmar')
 
 
 function getBotao2(number){
@@ -40,7 +19,7 @@ function getBotao2(number){
                 //Quando o quadrado de fundo estiver preenchido, ele vai remover uma classe, e vai adicionar a outro quadrado (no caso o proximo, representado por i+1)
                         }
                 break;
-                }
+                }        
         }   
 }
 
@@ -52,23 +31,17 @@ function corrige(){
                 }
         }
 }
-
-
 // o i vai percorrer todos os ifs, quando ele ver que o if é veradadeiro, ele vai executar o comando abaixo, e irá pro proximo
 
-function confirmar(){
 
-        for(i = 0; i < 4; i++){
+function confirmar(){
+          for(i = 0; i < 4; i++){
                 if(quadradoP[i].innerHTML != ""){
                  quadradoP[i].innerHTML = ""
                 }
         }
-        alert(`Voto enviado, seu canditado é ${nomePolitico}`)
-        display.innerHTML = "FIM!"
-        display.style.fontSize = "55px"
-        
-       
-        
+ alert(`Voto enviado, seu canditado é ${nomePolitico}`)
+ tela.innerHTML = "FIM!"
 }
 
 
@@ -78,18 +51,8 @@ function branco(){
                  quadradoP[i].innerHTML = ""
                 }
         }
-        display.innerHTML = "VOTO EM BRANCO!"
-        display.style.fontSize = "55px"
+ tela.innerHTML = "VOTO EM BRANCO"
 }
 
 
 
-/*
-function voltar(){
-display.innerHTML="Olá mundo!"
-setInterval(branco,confirmar,2000)
-}
-*/
-
-//fazer nome mudar apos ultimo digito digitado
-//desabilitar botoes de acao apos uma acao ocorrer (branco,confirmar)
