a232511593b120d21586d1cfbad2bf3c30f6c700
Vitor <vitorm.ramires@hotmail.com>
2021-04-28 18:04:41 -0300

    arrumando funções 03

--- script.js
+++ script.js
@@ -7,75 +7,97 @@
         var tela = document.querySelector('.display')
         var informacoes = document.querySelector('.informações')
         var Corrigir = document.querySelector('.Corrigir')
-        var emBranco = document.querySelector('.emBranco')
-        var Confirmar = document.querySelector('.Confirmar')
         var numero = document.querySelector('.numero')
 
 
 function getBotao2(number){
-        for(i = 0; i < 4; i++){
-                if(quadradoP[i].innerHTML == ""){
-                quadradoP[i].innerHTML = number
-                if(quadrado[i].innerHTML != ""){
-                                quadrado[i+1].classList.add('selecionando')
-                                quadrado[i].classList.remove('selecionando')  
-                //Quando o quadrado de fundo estiver preenchido, ele vai remover uma classe, e vai adicionar a outro quadrado (no caso o proximo, representado por i+1)
-                        }
-                break;
-                }
-        }     
-}
+
+  for(i = 0; i < 4; i++){
+    if(quadradoP[i].innerHTML == ""){
+        quadradoP[i].innerHTML = number
+    if(quadrado[i].innerHTML != ""){
+        quadrado[i+1].classList.add('selecionando')
+        quadrado[i].classList.remove('selecionando')  
+        //Quando o quadrado de fundo estiver preenchido, ele vai remover uma classe, e vai adicionar a outro quadrado (no caso o proximo, representado por i+1)
+        }
+        break;
+     }
+  }     
+}//botoes numerais
+
 
 function corrige(){
-        for(i = 4; i > 0; i--){
-                if(quadradoP[i-1].innerHTML != ""){
-                 quadradoP[i-1].innerHTML = ""
-                 quadrado[i-1].classList.add('selecionando')
-                 quadrado[i].classList.remove('selecionando')
-                 break
-                }
-        }
-}
-// o i vai percorrer todos os ifs, quando ele ver que o if é veradadeiro, ele vai executar o comando abaixo, e irá pro proximo
+
+   for(i = 4; i > 0; i--){
+    if(quadradoP[i-1].innerHTML != ""){
+        quadradoP[i-1].innerHTML = ""
+        quadrado[i-1].classList.add('selecionando')
+        quadrado[i].classList.remove('selecionando')
+        break
+     }
+   }
+}//botao de corrigir
+
 
 function confirmar(){
-          for(i = 0; i < 4; i++){ 
-                if(quadradoP[i].innerHTML != ""){
-                setTimeout (aparecerDisplay, 9000)
-                nomePolitico.innerHTML = "FIM!"
-                informacoes.style.display = "block"
-                numero.style.display = "none"
-                quadradoP[i].innerHTML = ""
-                } 
-                quadrado[i].classList.remove('selecionando')
-        }
-}    
+
+  var controle = true 
+   for(i = 0; i < 4; i++){
+     if(quadradoP[i].innerHTML == ""){
+          controle = false
+          break
+       } 
+    }
+    if(controle === false){
+      alert('Preencha os campos')
+    } else {    
+    setTimeout (aparecerDisplay, 3000)
+    setTimeout (aparecerImg,0)
+    nomePolitico.innerHTML = "FIM!"
+    informacoes.style.display = "block"
+    numero.style.display = "none"
+    document.getElementById("emBranco").disabled =  true;
+    for(i = 0; i < 4; i++){
+      quadradoP[i].innerHTML = ""
+      quadrado[i].classList.remove('selecionando')
+    }
+  }
+}//botao confirmar    
 
 function branco(){
-        for(i = 0; i < 4; i++) {
-                if(quadradoP[i].innerHTML != "") {
-                        quadradoP[i].innerHTML = ""
-                }
-        }
 
-nomePolitico.innerHTML = "VOTO EM BRANCO"
-informacoes.style.display = "none"
-numero.style.display = "none"
-imagem.style.display = "none"
-setTimeout (aparecerDisplay, 5000)   
+document.querySelectorAll('#botao').disabled = true;
+    for(i = 0; i < 4; i++) {
+      if(quadradoP[i].innerHTML != "") {
+        quadradoP[i].innerHTML = ""
+       }
+      quadrado[i].classList.remove('selecionando')
+    }
+        nomePolitico.innerHTML = "VOTO EM BRANCO"
+        informacoes.style.display = "none"
+        numero.style.display = "none"
+        imagem.style.display = "none"
+        setTimeout (aparecerDisplay, 3000)   
+document.getElementById("Confirmar").disabled = true;
+}//botao em branco
 
-}
 
 function aparecerDisplay(){
-nomePolitico.innerHTML = "Preencha os campos"
-informacoes.style.display = "none"
-numero.style.display = "block"
-imagem.style.display = "none"
-}
+
+        nomePolitico.innerHTML = "Preencha os campos"
+        informacoes.style.display = "none"
+        numero.style.display = "block"
+        imagem.style.display = "none"
+        document.getElementById("emBranco").disabled = false;
+        document.getElementById("Confirmar").disabled = false;
+}//aparecer o display inicial
 
 
 function aparecerImg(){
-imagem.style.display = "block"
-imagem.setAttribute('src', 'img.jfif')
-tela.appendChild(imagem)
-}
+  
+        imagem.style.display = "block"
+        imagem.setAttribute('src', 'img.jfif')
+        tela.appendChild(imagem)
+}//criar imagem
+
+
